4186c8fdf505204d03f50cf2ce27c58b723006db
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2023-07-15 03:25:19 +0700

    Updates

--- src/popup.ts
+++ src/popup.ts
@@ -19,7 +19,7 @@ export type PopupOptions = {
   swap?: boolean;
   /** Space (margin) to apply to the element position after positioned relative to the parent */
   space?: number;
-}
+};
 
 /**
  * Apply a fixed position bounds of the given element, relative to the given parent element.
@@ -36,112 +36,144 @@ export function popup(options: PopupOptions) {
     space = 8,
   } = options;
   if (!element || !parent) {
-    console.warn('Popup ignored because the given element/parent in the options is not an HTML Element.');
+    console.warn(
+      'Popup ignored because the given element/parent in the options is not an HTML Element.'
+    );
     return;
   }
 
   const { innerWidth, innerHeight } = window;
   const { width: elWidth, height: elHeight } = scaledBoundingClientRect(element, scale);
-  const { left, top, right, bottom, width, height, centerX, centerY } = offsets(parent, false, scale);
+  const { left, top, right, bottom, width, height, centerX, centerY } = offsets(
+    parent,
+    false,
+    scale
+  );
 
-  let styles: Partial<CSSStyleDeclaration> = {};
+  const styles: Partial<CSSStyleDeclaration> = {};
   let tx = '0';
   let ty = '0';
 
   if (xDir === 'before') {
-    const offSide = (right + elWidth) < 0;
+    const offSide = right + elWidth < 0;
 
     if (offSide) {
       if (swap) {
-        styles.left = `${ left + width }px`;
+        styles.left = `${left + width}px`;
       } else {
         styles.left = '0';
       }
 
-      styles.marginLeft = `${ space }px`;
+      styles.marginLeft = `${space}px`;
       element.classList.add('x-after');
     } else {
-      styles.right = `${ right + width }px`;
-      styles.marginRight = `${ space }px`;
+      styles.right = `${right + width}px`;
+      styles.marginRight = `${space}px`;
       element.classList.add('x-before');
     }
   } else if (xDir === 'after') {
-    const offSide = (left + elWidth) > innerWidth;
+    const offSide = left + elWidth > innerWidth;
 
     if (offSide) {
       if (swap) {
-        styles.right = `${ right + width }px`;
+        styles.right = `${right + width}px`;
       } else {
         styles.right = '0';
       }
 
-      styles.marginRight = `${ space }px`;
+      styles.marginRight = `${space}px`;
       element.classList.add('x-before');
     } else {
-      styles.left = `${ left + width }px`;
-      styles.marginLeft = `${ space }px`;
+      styles.left = `${left + width}px`;
+      styles.marginLeft = `${space}px`;
       element.classList.add('x-after');
     }
   } else if (xDir === 'between') {
-    styles.left = `${ centerX }px`;
-    tx = '-50%';
-    element.classList.add('x-between');
+    const offsideLeft = centerX - elWidth / 2 < 0;
+    const offsideRight = centerX + elWidth / 2 > innerWidth;
+
+    if (offsideLeft) {
+      styles.left = `${space}px`;
+      styles.maxWidth = `${innerWidth - space * 2}px`;
+      element.classList.add('x-screen-left');
+    } else if (offsideRight) {
+      styles.right = `${space}px`;
+      styles.maxWidth = `${innerWidth - space * 2}px`;
+      element.classList.add('x-screen-right');
+    } else {
+      styles.left = `${centerX}px`;
+      tx = '-50%';
+      element.classList.add('x-between');
+    }
   } else if (xDir === 'left') {
-    styles.left = `${ left }px`;
+    styles.left = `${left}px`;
     element.classList.add('x-left');
   } else if (xDir === 'right') {
-    styles.right = `${ right }px`;
+    styles.right = `${right}px`;
     element.classList.add('x-right');
   }
 
   if (yDir === 'above') {
-    const offSide = (bottom + elHeight) < 0;
+    const offSide = bottom + elHeight < 0;
 
     if (offSide) {
       if (swap) {
-        styles.top = `${ top + height }px`;
+        styles.top = `${top + height}px`;
       } else {
         styles.top = '0';
       }
 
-      styles.marginTop = `${ space }px`;
+      styles.marginTop = `${space}px`;
       element.classList.add('y-below');
     } else {
-      styles.bottom = `${ bottom + height }px`;
-      styles.marginBottom = `${ space }px`;
+      styles.bottom = `${bottom + height}px`;
+      styles.marginBottom = `${space}px`;
       element.classList.add('y-above');
     }
   } else if (yDir === 'below') {
-    const offSide = (top + elHeight) > innerHeight;
+    const offSide = top + elHeight > innerHeight;
 
     if (offSide) {
       if (swap) {
-        styles.bottom = `${ bottom + height }px`;
+        styles.bottom = `${bottom + height}px`;
         element.classList.add('y-below');
       } else {
         styles.bottom = '0';
       }
 
-      styles.marginBottom = `${ space }px`;
+      styles.marginBottom = `${space}px`;
       element.classList.add('y-above');
     } else {
-      styles.top = `${ top + height }px`;
-      styles.marginTop = `${ space }px`;
+      styles.top = `${top + height}px`;
+      styles.marginTop = `${space}px`;
       element.classList.add('y-below');
     }
   } else if (yDir === 'between') {
-    styles.top = `${ centerY }px`;
-    ty = '-50%';
-    element.classList.add('y-between');
+    const offsideTop = centerY - elHeight / 2 < 0;
+    const offsideBottom = centerY + elHeight / 2 > innerHeight;
+
+    if (offsideTop) {
+      styles.top = `${space}px`;
+      styles.maxHeight = `${innerHeight - space * 2}px`;
+      element.classList.add('y-screen-top');
+    } else if (offsideBottom) {
+      styles.bottom = `${space}px`;
+      styles.maxHeight = `${innerHeight - space * 2}px`;
+      element.classList.add('y-screen-bottom');
+    } else {
+      styles.top = `${centerY}px`;
+      ty = '-50%';
+      element.classList.add('y-between');
+    }
   } else if (yDir === 'top') {
-    styles.top = `${ top }px`;
+    styles.top = `${top}px`;
     element.classList.add('y-top');
   } else if (yDir === 'bottom') {
-    styles.bottom = `${ bottom }px`;
+    styles.bottom = `${bottom}px`;
     element.classList.add('y-bottom');
   }
 
-  styles.transform = `translate3d(${ tx }, ${ ty }, 0)`;
+  styles.transform = `translate3d(${tx}, ${ty}, 0)`;
   style(element, styles);
 }
 
@@ -185,7 +217,9 @@ export function restore(options: PopupOptions, debounce?: number) {
   const { element, parent } = options;
 
   if (!element || !parent) {
-    console.warn('Popup ignored because the given element/parent in the options is not an HTML Element.');
+    console.warn(
+      'Popup ignored because the given element/parent in the options is not an HTML Element.'
+    );
     return;
   }
 
